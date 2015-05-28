41fa8833cf72d1d7c85478dc9c4b91bb33559dca
tumanob <tumanob@gmail.com>
2015-05-28 17:59:23 +0600

    Making responsive - category page -  post page

--- wp-content/themes/TiLi/category.php
+++ wp-content/themes/TiLi/category.php
@@ -7,15 +7,18 @@
 		<div class="posts row">
 		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 			<div class="post col-xs-12 col-md-6 col-sm-6">
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-                <div class="sub"><span class="date"><?php the_time('d.m.Y') ?></span> | <span class="categories">Рубрика: <?php the_category(', ') ?></span></div>
+
                 <div class="row">
-                    <div class="col-xs-12 col-md-4 col-sm-4">
-                       <a href="<?php the_permalink() ?>" rel="bookmark" class="col-xs-6 col-md-12 col-sm-12" style="padding: 0px;"><?php the_post_thumbnail('thumb'); ?></a>
-                        <a href="<?php the_permalink() ?>" rel="bookmark" class="btn btn-danger btn-sm glyphicon glyphicon-download col-xs-6 col-md-12 col-sm-12"> Скачать</a> </button>
+                    <div class="col-xs-5 col-md-4 col-sm-4">
+                       <a href="<?php the_permalink() ?>" rel="bookmark" class="col-xs-8 col-md-12 col-sm-12" style="padding: 0px;"><?php the_post_thumbnail('thumb'); ?></a>
+                          <div class="sub col-xs-12 col-md-12 col-sm-12"> <span class="categories">Рубрика: <?php the_category(', ') ?></span></div>
+
                     </div>
-                    <div class="col-xs-12 col-md-8 col-sm-8">
-                        <?php the_excerpt(); ?>
+                    <div class="col-xs-7 col-md-8 col-sm-8">
+                         <?php //the_excerpt(); ?>
+                        <h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                        <a href="<?php the_permalink() ?>" rel="bookmark" class="btn btn-info btn-sm col-xs-12 col-md-12 col-sm-12"> Скачать</a>
+
                     </div>
                 </div>
 
--- wp-content/themes/TiLi/single.php
+++ wp-content/themes/TiLi/single.php
@@ -1,12 +1,20 @@
 <?php get_header(); ?>
 	<div id="wrapper">
 		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-			<div class="post">
-				<h2><?php the_title(); ?></h2>
-				<?php if(in_category('7')) {} else { ?>
-					<div class="sub"><span class="date"><?php the_time('d.m.Y') ?></span> | <span class="categories">Рубрика: <?php the_category(', ') ?></span></div>
-				<?php } ?>
-				<?php the_content(); ?>				
+			<div class="post ">
+				<h1><?php the_title(); ?></h1>
+                <?php if(in_category('7')) {} else { ?>
+                                    					<div class="sub"><span class="date"><?php the_time('d.m.Y') ?></span> | <span class="categories">Рубрика: <?php the_category(', ') ?></span></div>
+                                    				<?php } ?>
+                <div class="col-xs-12 col-md-4 col-sm-12">
+                    <?php the_post_thumbnail('medium'); ?>
+                </div>
+                <div class="col-xs-12 col-md-8 col-sm-12">
+                    <?php the_content(); ?>
+                </div>
+
+
+
 			</div>
 			<?php comments_template( '', true ); ?>
             <?php endwhile; else : endif; ?>            
