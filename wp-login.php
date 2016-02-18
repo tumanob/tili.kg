0edd6cb48f2e4904868a8d567be3b8a5623641c1
tumanob <tumanob@gmail.com>
2016-02-18 14:10:48 +0600

    #18 - сверстать в стиле форму логина -  регистрации - восстановления пароля

--- wp-login.php
+++ wp-login.php
@@ -98,8 +98,8 @@ function login_header( $title = 'Log In', $message = '', $wp_error = '' ) {
 		$login_header_url   = network_home_url();
 		$login_header_title = get_current_site()->site_name;
 	} else {
-		$login_header_url   = __( 'https://wordpress.org/' );
-		$login_header_title = __( 'Powered by WordPress' );
+		$login_header_url   = __( 'http://tili.kg' );
+		$login_header_title = __( 'Проект Tili.kg' );
 	}
 
 	/**
